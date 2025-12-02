c86513748f2baa77f330d7df7ac9483e5d03deba
tiffanytitania <165647669+tiffanytitania@users.noreply.github.com>
2025-12-02 19:57:15 +0700

    Commit No. 3 – Upgrading app with flow

--- app/src/main/java/com/example/test_lab_week_12/MovieRepository.kt
+++ app/src/main/java/com/example/test_lab_week_12/MovieRepository.kt
@@ -4,25 +4,18 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.test_lab_week_12.api.MovieService
 import com.example.test_lab_week_12.model.Movie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 
 class MovieRepository(private val movieService: MovieService) {
     private val apiKey = "d865328401d4124e72a4a22f46e021d6"
 
     private val movieLiveData = MutableLiveData<List<Movie>>()
-    val movies: LiveData<List<Movie>>
-        get() = movieLiveData
 
-    private val errorLiveData = MutableLiveData<String>()
-    val error: LiveData<String>
-        get() = errorLiveData
-
-    suspend fun fetchMovies() {
-        try {
-            val popularMovies = movieService.getPopularMovies(apiKey)
-            movieLiveData.postValue(popularMovies.results)
-        } catch (exception: Exception) {
-            errorLiveData.postValue(exception.message ?: "Unknown error")
-            exception.printStackTrace()
-        }
-    }
+    fun fetchMovies() = flow {
+        val response = movieService.getPopularMovies(apiKey)
+        emit(response.results)
+    }.flowOn(Dispatchers.IO)
 }
--- app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
+++ app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
@@ -1,25 +1,38 @@
 package com.example.test_lab_week_12
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.test_lab_week_12.model.Movie
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.collect
 
-class MovieViewModel(private val movieRepository: MovieRepository) : ViewModel() {
+class MovieViewModel(
+    private val movieRepository: MovieRepository
+) : ViewModel() {
+
+    private val _popularMovies = MutableStateFlow(emptyList<Movie>())
+    val popularMovies: StateFlow<List<Movie>> = _popularMovies
+
+    private val _error = MutableStateFlow("")
+    val error: StateFlow<String> = _error
+
     init {
-        fetchPopularMovies()
+        loadMovies()
     }
 
-    val popularMovies: LiveData<List<Movie>>
-        get() = movieRepository.movies
-    val error: LiveData<String>
-        get() = movieRepository.error
-
-    private fun fetchPopularMovies() {
-        viewModelScope.launch(Dispatchers.IO) {
+    private fun loadMovies() {
+        viewModelScope.launch {
             movieRepository.fetchMovies()
+                .catch { e ->
+                    _error.value = e.message ?: "Unknown error"
+                }
+                .collect { movies ->
+                    _popularMovies.value = movies
+                }
         }
     }
 }
