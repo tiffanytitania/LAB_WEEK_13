db79a7c7af4411f9695155a4f7711483f171c066
tiffanytitania <165647669+tiffanytitania@users.noreply.github.com>
2025-12-03 15:56:25 +0700

    Commit No. 1 – add data binding

--- app/src/main/java/com/example/lab_week_13/RecyclerViewBinding.kt
+++ app/src/main/java/com/example/lab_week_13/RecyclerViewBinding.kt
@@ -3,7 +3,7 @@ package com.example.lab_week_13
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lab_week_13.model.Movie
-
+//data binding
 @BindingAdapter("list")
 fun bindMovies(view: RecyclerView, movies: List<Movie>?) {
     val adapter = view.adapter as MovieAdapter
